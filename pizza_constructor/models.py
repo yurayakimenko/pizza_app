eae76f22b0e31fda8c016f2b2cecebd002a896f9
yurayakimenko <yakimenkomusic@gmail.com>
2019-02-27 18:22:10 +0200

    static constructor form implemented

--- pizza_constructor/models.py
+++ pizza_constructor/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class IngredientGroup(models.Model):
     name = models.CharField(max_length=100)
+    objects = models.Manager()
 
     def __str__(self):
         return self.name
@@ -11,7 +12,9 @@ class IngredientGroup(models.Model):
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
     price = models.FloatField()
-    group = models.ForeignKey(IngredientGroup, on_delete=models.CASCADE)
+    group = models.ForeignKey(IngredientGroup, related_name="ingredients",
+                              on_delete=models.CASCADE)
+    objects = models.Manager()
 
     def __str__(self):
         return "{} | {} | {}".format(self.name, self.group, self.price)
@@ -23,3 +26,6 @@ class Order(models.Model):
     phone = models.CharField(max_length=20)
     name = models.CharField(max_length=100)
     amount = models.FloatField()
+    confirmed = models.BooleanField(default=False)
+
+    objects = models.Manager()
--- pizza_constructor/views.py
+++ pizza_constructor/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import IngredientGroup
 
 
 def pizza_constructor_page(request):
-    return render(request, 'pizza_constructor/constructor_form.html', {})
+    ingredient_groups = IngredientGroup.objects.all()
+    return render(request, 'pizza_constructor/constructor_form.html',
+                  {"title": "Выбери ингредиенты пиццы",
+                   "ingredient_groups": ingredient_groups})
 
